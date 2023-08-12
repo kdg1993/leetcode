88178d4474a7724d25f9cb7049d4bcf12a9647cd
kdg1993 <daegunkim0425@gmail.com>
2023-08-12 14:40:57 +0900

    [SQL MEDIUM 1158] Market Analysis I

---
+++ SQL/MEDIUM/SQL_1158. Market Analysis I.sql
@@ -0,0 +1,6 @@
+select u.user_id as buyer_id, u.join_date, ifnull(count(o.order_id), 0)as orders_in_2019
+from Users as u
+left join Orders as o
+on u.user_id = o.buyer_id and year(o.order_date) = 2019
+group by u.user_id
+order by u.user_id asc
