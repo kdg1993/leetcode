f7ff12d3f666f3ffa943a6edf862be4f122e71f0
kdg1993 <daegunkim0425@gmail.com>
2023-10-22 17:16:24 +0900

    [SQL MEDIUM 1174] Immediate Food Delivery II

---
+++ SQL/MEDIUM/SQL_1174. Immediate Food Delivery II.sql
@@ -0,0 +1,10 @@
+select round(100*avg(
+  case when order_date = customer_pref_delivery_date then 1 else 0 end
+), 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+  select customer_id, min(order_date)
+  from Delivery
+  group by customer_id
+  )
+;
